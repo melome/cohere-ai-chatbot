b63402584b7eceaf5a66dbcb76649f91427a7ff0
melome <shanleyrosero@gmail.com>
2024-07-06 05:32:42 +0800

    style: customized scrollbar using globalStyles

---
+++ client/src/globalStyles.js
@@ -0,0 +1,17 @@
+const globalStyles = {
+    '*::-webkit-scrollbar': {
+        width: '8px',
+    },
+    '*::-webkit-scrollbar-thumb': {
+        backgroundColor: '#0C3E43',
+        borderRadius: '8px',
+    },
+    '*::-webkit-scrollbar-thumb:hover': {
+        backgroundColor: '#32757C',
+    },
+    '*::-webkit-scrollbar-track': {
+        backgroundColor: '#f1f1f1',
+    },
+};
+
+export default globalStyles;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx';
 import './index.css';
 import theme from './theme';
+import globalStyles from './globalStyles';
+import { GlobalStyles } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       {/* <CssBaseline /> */}
+      <GlobalStyles styles={globalStyles} />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
