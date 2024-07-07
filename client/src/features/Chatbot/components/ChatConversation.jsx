6205e2842e238e3d7d3a8300414e40cf98172863
melome <shanleyrosero@gmail.com>
2024-07-08 00:28:33 +0800

    style: refactor code and used styled-components

--- client/src/features/Chatbot/components/ChatConversation.jsx
+++ client/src/features/Chatbot/components/ChatConversation.jsx
@@ -4,48 +4,59 @@ import styled from 'styled-components';
 import useStore from '../../../helpers/store';
 import { setLocalStorage } from '../../../helpers/localstorage';
 
-const StyledBox = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  overflow-y: auto;
-  padding: 2;
-  background-color: #f5f5f5;
-  height: 300;
-  max-height: 300;
-`;
-
-const Message = styled(Typography)`
-  word-wrap: break-word;
-`;
-
 const ChatConversation = () => {
   const { chat_history } = useStore();
   setLocalStorage('chat_history', chat_history);
   return (
     <StyledBox>
       {chat_history.map((chat, index) => (
-        <Paper
+        <Message
           key={index}
-          elevation={0}
-          sx={{
-            margin: 1,
-            padding: 1.5,
-            paddingLeft: 2,
-            paddingRight: 2,
-            borderRadius: 3,
-            maxWidth: '60%',
-            textAlign: chat.role === 'USER' ? 'right' : 'left',
-            alignSelf: chat.role === 'USER' ? 'flex-end' : 'flex-start',
-            backgroundColor: chat.role === 'USER' ? '#32757c' : '#ffffff',
-            color: chat.role === 'USER' ? '#ffffff' : '#32757c',
-          }}
+          elevation={1}
+          className={chat.role === 'USER' ? 'userMessage' : 'chatbotMessage'}
         >
-          <Message>{chat.message}</Message>
-        </Paper>
+          <MessageContent>{chat.message}</MessageContent>
+        </Message>
       ))}
     </StyledBox>
   );
 };
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-y: auto;
+  padding: 8px;
+  background-color: #f5f5f5;
+  height: 300px;
+  max-height: 300px;
+`;
+
+const Message = styled(Paper)`
+  margin: 4px 8px;
+  padding: 8px 16px;
+  border-radius: 8px;
+  max-width: 60%;
+  word-wrap: break-word;
+
+  &.userMessage {
+    text-align: right;
+    align-self: flex-end;
+    background-color: #32757c;
+    color: #ffffff;
+  }
+
+  &.chatbotMessage {
+    text-align: left;
+    align-self: flex-start;
+    background-color: #ffffff;
+    color: #32757c;
+  }
+`;
+
+const MessageContent = styled(Typography)`
+  word-wrap: break-word;
+`;
 
 export default ChatConversation;
